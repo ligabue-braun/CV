11607b1d1ef1a473c25742a59acc0085464c4365
ligabue-braun <ligabue.braun@gmail.com>
2017-07-05 20:51:02 -0300

    Add files via upload

---
+++ motivatequote_v2.js
@@ -0,0 +1,69 @@
+
+var quotes=new Array()
+
+//change the quotes if desired. Add/ delete additional quotes as desired.
+
+quotes[0]='Margaret Oakley Dayhoff (1925-1983) is considered the Mother of Bioinformatics'
+
+quotes[1]='Chronologically, a <i>T. rex</i> is closer to us than it is close to a <i>Stegosaurus</i>.'
+
+quotes[2]='There is a hidden message coded in the dinosaur genes from the Jurassic Park sequel, The Lost World.'
+
+quotes[3]='There are at least four kinds of venomous mammals.'
+
+quotes[4]='Bats are not mentioned in the original Dracula novel by Bram Stoker.'
+
+quotes[5]='Rats laugh when tickled.'
+
+quotes[6]='Wombat poop is cube-shaped.'
+
+quotes[7]='Keas are flesh-eating parrots from New Zealand.'
+
+quotes[8]='Lesch-Nyhan syndrome induces the affected individual, among other things, to eat their own nose, lips, and tongue.'
+
+quotes[9]='Orcas in captivity have learned independently how to hunt seabirds using fish baits.'
+
+quotes[10]='The cassowary is considered the most dangerous bird to humans.'
+
+quotes[11]='There are numerous poisonous birds.'
+
+quotes[12]='Both cheetahs and Tasmanian devils suffered intense populational bottleneck effects, being almost homogenous genetically.'
+
+quotes[13]='The popular <i>Triceratops</i> may be an invalid dinosaur genus, being considered by some as a juvenile form of other genera.'
+
+quotes[14]='The research on antidepressants had almost no improvement for the last fifty years.'
+
+quotes[15]='<i>Theobroma</i>, the genus of cocoa plant, means \'food of the gods\'.'
+
+quotes[16]='The giant panda scientific name, <i>Ailuropoda melanoleuca</i>, means \'cat paws in black and white\'.'
+
+quotes[17]='Bestowing meaning to symbols is considered the only human-exclusive feature, since culture and language are found in many other animals.'
+
+quotes[18]='Paleobiochemistry is growing as a research field since the discovery that dinosaur fossils hold considerable amounts of preserved biological material.'
+
+quotes[19]='Ten years after confirming that enzymes are proteins, James Summner received a Nobel Prize. Prior to that, he was publicly called \'stupid\' for his propositions.'
+
+quotes[20]='Wegener died still being ridiculed for proposing the plate tectonics theory, something now taught to children as obvious.'
+
+quotes[21]='Titanium dioxide is used to provide luminescent white coloring to food.'
+
+quotes[22]='The patent for a new cancer treatment forbade anyone of thinking about it without paying royalties. The absurdity of such claim had to be legally questioned prior to its invalidation.'
+
+quotes[23]='There is still no confirmed vector for the Ebola virus. Many insects, rodents, and bats have been proposed to transmit the disease, but no confirmation has been obtained so far.'
+
+quotes[24]='Despite its wide usage along with <i>in vitro</i> and <i>in vivo</i>, <i>in silico</i> is not gramatically correct. <i>In silicium</i> would be the adequate form.'
+
+quotes[25]='Despite their popular representation, the A:T, C:G base pairs in DNA are not mandatory. Called Watson-Crick pairs, they are only the most studied ones. alternate pairs include the Hoogsten pairs.'
+
+quotes[26]='The Dunning-Kruger effect describes how the most ignorant people tend to consider themselves intelectually superior to their peers.'
+
+quotes[27]='The Mandela effect describes collective false memories, such as the general belief that Nelson Mandela was already dead for many years when he passed away.'
+
+quotes[28]='There is ongoing debate on wether Komodo dragons are venomous or not.'
+
+quotes[29]='Primates and bats are the only mammals unable to synthesize their own vitamin C.'
+
+quotes[30]='Koalas, pandas, and chameleons have \'fake\' thumbs, since true thumbs are only found in primates.'
+
+var whichquote=Math.floor(Math.random()*(quotes.length))
+document.write(quotes[whichquote])
